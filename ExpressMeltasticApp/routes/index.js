7a92575d3ada08c3fd5274b2b91107e8d1f26373
meltastic <mhargis@csusm.edu>
2015-05-05 20:27:34 -0700

    initial commit

---
+++ ExpressMeltasticApp/routes/index.js
@@ -0,0 +1,21 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+};
+
+exports.myword = function (req, res) {
+    console.log(req.body);
+    res.render('myword', { title: 'My Word', year: new Date().getFullYear(), message: "Here's your word!" });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page.' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page.' });
+};
